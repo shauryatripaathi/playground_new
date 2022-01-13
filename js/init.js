e39cd28df8918278c003078c60f04444babc354b
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-01-13 23:49:02 +0530

    updated

---
+++ js/init.js
@@ -0,0 +1,130 @@
+let tileIds = [
+    "tile11",
+    "tile12",
+    "tile13",
+    "tile21",
+    "tile22",
+    "tile23",
+    "tile31",
+    "tile32",
+    "tile33"];
+
+let gridBox = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+for (let x of tileIds) {
+    myFunc(x);
+}
+
+let nextMove = "X";
+
+function myFunc(item) {
+    let val = document.getElementById(item);
+    
+    val.addEventListener("click", handleOnClick);
+    function handleOnClick(e) {
+        let getImg = document.createElement("img");
+        getImg.classList.add("getImg");
+        getImg.src = nextMove === "X" ? "/img/images.png" : "/img/images (1).png";
+        e.target.append(getImg);    
+        
+
+        // next move
+        nextMove = nextMove === "X" ? "R" : "X";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunc() {
+//     console.log("hello")
+// }
+// myFunc();
+
+// // anonymous function
+// let x = function () {
+//     console.log("this is anonymous function")
+// }
+// x();
+
+// // pass function as argument
+// function greet() {
+//     console.log("good mornning");
+// }
+// function msg(message) {
+//     console.log(message());
+// }
+// msg(greet);
+
+
+// // function inside a function
+// function sayHi() {
+//     function helloFunc() {
+//         console.log("hii person");
+//     }
+//     console.log(helloFunc());
+// }
+// sayHi();
+
+
+// // double parantheses
+// function dblPar() {
+//     return function myFunction() {
+//         console.log("this is an example of double paranthese");
+//     }
+// }
+// dblPar()();
+
+// // self invoking function
+// (function () {
+//     console.log("this is a self-invoking function");
+// })();
+
+
+// // nested function
+// let num1 = 10;
+// let num2 = 90;
+// function add() {
+//     function answer() {
+//         console.log(num1 + num2);
+//     }
+//     return answer();
+// }
+// add();
+
+// //Multiply-nested function
+// function A(x) {
+//     function B(y) {
+//         function C(z) {
+//             console.log(x + y + z);
+//         }
+//         C(10);
+//     }
+//     B(20);
+// }
+// A(30);
+
+
+// // arrow function
+// const arr = [1, 2, 3, 4];
+// const arr1 = arr.map(i => {
+//     console.log(i ** 2);
+// });
+
+
+
